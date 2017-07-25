417aa815042fdfa0d4ea92b91edca777e5c22406
alexgriff <alexalexgriffith@gmail.com>
2017-07-25 15:35:49 -0400

    post lecture

--- app/models/student.rb
+++ app/models/student.rb
@@ -1,3 +1,18 @@
 class Student < ApplicationRecord
-  validates :email, :name, presence: true
+  validates :email, :name, :dob, presence: true
+
+  # student must be over 18 yrs Date.today - 18.years > dob
+  validate :must_be_eighteen
+
+  # emails must be unique
+  validates :email, uniqueness: true
+  # students name cannot be 'Drop Table'
+  validates :name, exclusion: {in: ['Drop Table']}
+
+  def must_be_eighteen
+    if self.dob && !(Date.today - 18.years  > self.dob)
+      self.errors.add(:dob, 'must be older than 18')
+    end
+  end
+
 end
